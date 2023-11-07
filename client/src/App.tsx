4581af7b8b6275bed193dd1c88e9b639ad429e84
Baptiste Boimard <bouketin27@gmail.com>
2023-11-07 23:30:53 +0100

    Essaye de commit

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.scss';
 import BodyBox from './app/components/BodyBox/bodyBox';
 
+// Composant principale de l'application
 function App() {
   return (
     <div className="App">
