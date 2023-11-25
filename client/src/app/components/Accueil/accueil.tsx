46add20ead475e9518e9cb2cef6399663489643b
baptiste-boimard <bouketin27@gmail.com>
2023-11-25 19:39:44 +0100

    essai de compose uniquement avce les images

--- client/src/app/components/Accueil/accueil.tsx
+++ client/src/app/components/Accueil/accueil.tsx
@@ -11,7 +11,7 @@ function Accueil () {
     color={'black'}
     textAlign={'center'}
   >
-    <h1>00Bienvenue dans l'inventaire de la NWS</h1>
+    <h1>Bienvenue dans l'inventaire de la NWS</h1>
     <p>Choisissez la rubrique qui vous intérense</p>
   </Box>
   );
